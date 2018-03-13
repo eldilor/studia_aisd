d80cc771d57aa2d965641681ff30d7b90cf19cf3
Oskar Kupski <oskar.kupski@gmail.com>
2018-03-13 11:28:10 +0100

    task -> 3 menu added

--- src/Lab3/App.java
+++ src/Lab3/App.java
@@ -2,11 +2,68 @@ package Lab3;
 
 import services.Helper;
 
+import java.util.Scanner;
+
 public class App {
     public static void run() {
-        SinglyLinkedList<Card> deck = App.generateRandomSortedSinglyLinkedDeck();
+        SinglyLinkedList<Card> deck = new SinglyLinkedList<Card>();
+        Scanner scanner = new Scanner(System.in);
+        App.displayMenu();
 
-        deck.display();
+        while (scanner.hasNext()) {
+            int option = scanner.nextInt();
+
+            if (option == 0) {
+                break;
+            }
+
+            switch (option) {
+                case 1:
+                    deck = App.generateRandomSortedSinglyLinkedDeck();
+                    break;
+                case 2:
+                    if (deck != null) {
+                        deck.display();
+                    }
+                    break;
+                case 3:
+                    if (deck != null) {
+                        System.out.println("Liczba Elementów: " + deck.size());
+                    }
+                    break;
+                case 4:
+                    System.out.println("Podaj wartość <1, 13>");
+                    int value = scanner.nextInt();
+                    for (int i = 0; deck != null && i < deck.size(); i++) {
+                        Card card = deck.get(i);
+                        if (card.getValue() == value) {
+                            System.out.println(card);
+                        }
+                    }
+                    break;
+                case 5:
+                    System.out.println("Podaj kolor <0, 3>");
+                    int color = scanner.nextInt();
+                    for (int i = 0; deck != null && i < deck.size(); i++) {
+                        Card card = deck.get(i);
+                        if (card.getColor() == color) {
+                            System.out.println(card);
+                        }
+                    }
+                    break;
+            }
+
+            App.displayMenu();
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("\n0 - zakończ program");
+        System.out.println("1 - utwórz liste");
+        System.out.println("2 - wyświetl liste");
+        System.out.println("3 - wyświetl liczbe elementow");
+        System.out.println("4 - wyswietl karty z wartoscia");
+        System.out.println("5 - wyswietl karty z kolorem");
     }
 
     private static SinglyLinkedList<Card> generateRandomSortedSinglyLinkedDeck() {
